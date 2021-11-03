354c4eea95267ddcc1fc21bf2a35c4f0227e729d
MMedikov <max.mediko03@gmail.com>
2021-11-03 22:01:14 +0300

    Реализована функция decrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,7 +15,16 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in range(len(plaintext)):
+        char = plaintext[i]
+        code = ord(char)
+        if (code >= 65 and code <= 90) or (code >= 97 and code <= 255): 
+            if (char.isupper()): 
+                ciphertext += chr((code + (shift - 65)) % 26 + 65) 
+            else: 
+                ciphertext += chr((code + (shift - 97)) % 26 + 97) 
+        else: 
+            ciphertext += chr(code) 
     return ciphertext
 
 
@@ -33,7 +42,16 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in range(len(ciphertext)):
+        char = ciphertext[i]
+        code = ord(char)
+        if (code >= 65 and code <= 90) or (code >= 97 and code <= 255):
+            if (char.isupper()):
+                plaintext += chr((ord(char) - (shift + 65)) % 26 + 65)
+            else:
+                plaintext += chr((ord(char) - (shift + 97)) % 26 + 97) 
+        else:
+            plaintext += chr(code) 
     return plaintext
 
 
@@ -42,5 +60,9 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     Brute force breaking a Caesar cipher.
     """
     best_shift = 0
-    # PUT YOUR CODE HERE
+    for i in range(0,26):
+        plain_text = decrypt_caesar(ciphertext, i) 
+        if plain_text in dictionary:
+           best_shift = i
+    
     return best_shift
