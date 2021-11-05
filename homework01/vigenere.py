bdf65b3bb8131360cec5e19623e178a58f9ddc23
MMedikov <max.mediko03@gmail.com>
2021-11-05 19:12:23 +0300

    Реализован шифр Виженера()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,8 +9,21 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
+    key = generateKey(plaintext, keyword)
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in range(len(plaintext)):
+        charCode = ord(plaintext[i])
+        shift = ord(key[i])
+        if (charCode >= 65 and charCode <= 90) or (charCode >= 97 and charCode <= 255):
+            if (plaintext[i].isupper()):
+                base = 65
+            else:
+                base = 97
+
+            x = ((charCode - base + shift - base) % 26) + base
+            ciphertext += chr(x)
+        else:
+            ciphertext += chr(charCode)
     return ciphertext
 
 
@@ -25,6 +38,28 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
+    key = generateKey(ciphertext, keyword)
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in range(len(ciphertext)):
+        charCode = ord(ciphertext[i])
+        shift = ord(key[i])
+        if (charCode >= 65 and charCode <= 90) or (charCode >= 97 and charCode <= 255):
+            if (ciphertext[i].isupper()):
+                base = 65
+            else:
+                base = 97
+
+            x = ((charCode - shift) % 26) + base
+            plaintext += chr(x)
+        else:
+            plaintext += chr(charCode)
     return plaintext
+
+def generateKey(string, key):
+    key = list(key)
+    if len(string) == len(key):
+        return(key)
+    else:
+        for i in range(len(string) - len(key)):
+            key.append(key[i % len(key)])
+    return("" . join(key))
